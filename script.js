41a0b293f2516f952cdc9d2224cb2ed81a75a547
gbitencorte <giovana.bitencorte@aluno.senai.br>
2025-06-10 17:24:37 -0300

    lista de tarefas funcional

--- script.js
+++ script.js
@@ -9,5 +9,32 @@ titulo.innerHTML = `Lista de tarefas: ${nome}`;
 // acompanha o evento de clique do botão adicionar tarefa
 btnAdd.addEventListener("click", criarTarefa)
 function criarTarefa() {
-    
+    const listItem = document.createElement('li')
+    listItem.textContent = tarefa.value;
+    taskList.appendChild(listItem);
+    // criar botão de deletar tarefa
+    const removeButton = document.createElement('button');
+    removeButton.id = "remove";
+    removeButton.textContent = "x"
+    listItem.appendChild(removeButton)
+
+    removeButton.addEventListener("click", function(){
+        taskList.removeChild(listItem)
+    })
+
+    // criar botão de concluir tarefa
+    const concluirButton = document.createElement('button');
+    concluirButton.id = "conclui";
+    concluirButton.textContent = "✓"
+
+    let buttonsItem = document.createElement('div')
+    buttonsItem.classList.toggle('buttonsItem')
+    listItem.appendChild(buttonsItem)
+    buttonsItem.appendChild(concluirButton)
+    buttonsItem.appendChild(removeButton)
+
+    concluirButton.addEventListener("click", function(){
+        listItem.classList.toggle('completed')
+        // concluirButton.style.color = "green"
+    })
 }
